a4451cdc62b979875b1ac35af56886340fbdf8ae
SheshkoPavel <derzurb@mail.ru>
2022-06-29 17:57:44 +0300

    feat: del test and default files and create in again

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from "@nestjs/core";
+import {AppModule} from "./app.module";
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+const cors = require('cors');
+
+async function start () {
+    const PORT = process.env.PORT || 5000;
+    const app = await NestFactory.create(AppModule);
+
+    //Возможность отсылать запросы на локальный сервер
+    app.use(cors({
+        origin: 'http://localhost:3000',
+        methods: ['POST', 'PUT', 'GET', 'PATCH', 'OPTIONS', 'HEAD', 'DELETE'],
+        credentials: true,
+    }));
+
+
+    await app.listen(PORT, ()=> console.log(`Server started at port = ${PORT}`));
 }
-bootstrap();
+
+start ();
