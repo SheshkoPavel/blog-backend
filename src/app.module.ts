51bdda3db3fd071ff3d92e7ddbd38752871cc663
SheshkoPavel <derzurb@mail.ru>
2022-06-30 13:41:37 +0300

    feat: add DB settings and development and production .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,22 @@
 import {Module} from "@nestjs/common";
 import {SequelizeModule} from "@nestjs/sequelize";
 import { UsersModule } from './users/users.module';
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
     controllers: [],
     providers: [],
     imports: [
+        ConfigModule.forRoot({
+            envFilePath: `.${process.env.NODE_ENV}.env`  //Отслеживание режима пользователь/разработчик
+        }),
         SequelizeModule.forRoot({   //Найстройка Type ORM Sequelize
             dialect: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            username: 'postgres',
-            password: 'sheshko12091309',
-            database: 'blog',
+            host: process.env.POSTGRES_HOST,
+            port: Number(process.env.POSTGRESS_PORT),
+            username: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
+            database: process.env.POSTGRES_DB,
             models: [],
             autoLoadModels: true
         }),
