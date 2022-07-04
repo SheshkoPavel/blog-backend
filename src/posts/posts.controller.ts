87090ef6da5ba16801f4a46f7198cd2b0542627b
SheshkoPavel <derzurb@mail.ru>
2022-07-04 19:48:09 +0300

    feat: add guards to crate and edit posts

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,9 +1,10 @@
-import {Body, Controller, Get, HttpStatus, Patch, Post, UploadedFile, UseInterceptors} from '@nestjs/common';
+import {Body, Controller, Get, HttpStatus, Patch, Post, UploadedFile, UseGuards, UseInterceptors} from '@nestjs/common';
 import {CreatePostDto} from "./dto/create-post.dto";
 import {PostsService} from "./posts.service";
 import {ApiOperation, ApiResponse, ApiTags} from "@nestjs/swagger";
 import {FileInterceptor} from "@nestjs/platform-express";
 import {UpdatePostDto, UpdatePostResponseDto} from "./dto/update-post.dto";
+import {JwtAuthGuard} from "../auth/jwt-auth.guard";
 
 
 @ApiTags('Доступные запросы для Статей блога')
@@ -12,6 +13,7 @@ export class PostsController {
 
     constructor(private postService: PostsService) {}
 
+    @UseGuards(JwtAuthGuard) // Ограничение доступа к эндпоинту с помощью Guard
     @Post()
     @UseInterceptors(FileInterceptor('image'))
     createPost(@Body() dto: CreatePostDto, @UploadedFile() image){
@@ -27,6 +29,7 @@ export class PostsController {
 
     @ApiOperation({summary: 'Изменение статьи по id', description: 'В ответе вы получите responseCode и сообщение'})
     @ApiResponse({status: HttpStatus.OK, type: UpdatePostResponseDto})
+    @UseGuards(JwtAuthGuard)
     @Patch()
     async updatePost(@Body() updatePostDto: UpdatePostDto): Promise<UpdatePostResponseDto>{
         const [updateResponseCode] = await this.postService.updatePost(updatePostDto);
