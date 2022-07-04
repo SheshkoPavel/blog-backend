c40398287708940ac2b877f6803be3b501c7bb07
SheshkoPavel <derzurb@mail.ru>
2022-07-04 13:04:27 +0300

    fix: image in post

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,8 @@
-import {Body, Controller, Post, UploadedFile} from '@nestjs/common';
+import {Body, Controller, Post, UploadedFile, UseInterceptors} from '@nestjs/common';
 import {CreatePostDto} from "./dto/create-post.dto";
 import {PostsService} from "./posts.service";
 import {ApiTags} from "@nestjs/swagger";
+import {FileInterceptor} from "@nestjs/platform-express";
 
 @ApiTags('Доступные запросы для Статей блога')
 @Controller('posts')
@@ -10,6 +11,7 @@ export class PostsController {
     constructor(private postService: PostsService) {}
 
     @Post()
+    @UseInterceptors(FileInterceptor('image'))
     createPost(@Body() dto: CreatePostDto, @UploadedFile() image){
         return this.postService.create(dto, image);
     }
