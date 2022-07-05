b3adff2b57ba92f2c8ce73b66666334cbd500dbb
SheshkoPavel <derzurb@mail.ru>
2022-07-05 16:11:01 +0300

    fix: small fix

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -7,6 +7,7 @@ import {UpdatePostDto} from "./dto/update-post.dto";
 import {AddTagDto} from "../roles/dto/add-tag.dto";
 import {TagsService} from "../tags/tags.service";
 
+
 @Injectable()
 export class PostsService {
 
